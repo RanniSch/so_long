b359a7a60aa376c1e7f7a412e5d936ee249a4d87
rschlott <rschlott@student.42wolfsburg.de>
2022-12-10 09:05:59 +0100

    new

--- read_map.c
+++ read_map.c
@@ -6,7 +6,7 @@
 /*   By: rschlott <rschlott@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 19:45:00 by rschlott          #+#    #+#             */
-/*   Updated: 2022/12/10 08:46:15 by rschlott         ###   ########.fr       */
+/*   Updated: 2022/12/10 09:03:31 by rschlott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,25 @@ int open_file(t_data *data, char **argv)
     return(fd);
 }
 
+void    count_rows(t_data *data, char *line)
+{
+    if (line)
+        data->columns = ft_strlen(line) - 1;
+    else
+    {
+        free(data);
+        //missing something here
+    }
+    printf("%d\n", data->columns);
+}
+
 void    read_the_map(t_data *data, char **argv)
 {
-    int fd;
+    int     fd;
+    char    *line;
 
     fd = open_file(data, argv);
     printf("%d\n", fd);
+    line = get_next_line(fd);
+    count_rows(data, line);
 }
